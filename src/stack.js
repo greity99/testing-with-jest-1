544c47c0fed35122d44e87c9c2655ffd15c00043
greity99 <nahar@hotmail.se>
2025-05-14 21:26:20 +0200

    Fixade peak

--- src/stack.js
+++ src/stack.js
@@ -1,18 +1,18 @@
-var _ = require('underscore');
+const _ = require('underscore');
 
 let stack = [];
 
 // Lägger ett element överst i stacken
 exports.push = function (x) {
-  stack.push(x);
+    stack.push(x);
 };
 
 // Returnerar det översta elementet i stacken och tar bort det
 exports.pop = function () {
-  return stack.pop();
+    return stack.pop();
 }
 
 // Returnerar det översta elementet i stacken
 exports.peek = function () {
-  return _.last(stack);
+    return stack[stack.length - 1]; // Åtgärdat: returnerar det sista elementet i stacken
 }
